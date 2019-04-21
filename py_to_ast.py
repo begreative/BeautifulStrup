237b678df02e49711b64e96e86e852a4af496d77
Ben Arbib <ba2490@columbia.edu>
2019-04-21 01:51:33 -0400

    added py file that takes in dir and creates txt files of asts

---
+++ py_to_ast.py
@@ -0,0 +1,31 @@
+import ast
+import astpretty
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print("Enter directory path")
+    exit()
+
+d = sys.argv[1]
+directory = os.fsencode(d)
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".py"):
+        python_file = (os.path.join(directory.decode("utf-8"), filename))
+
+        in_file = open(python_file, "r")
+        code = in_file.readlines()
+        code = "".join(code)
+        in_file.close()
+
+        ast_node = ast.parse(code)
+        pretty_string = astpretty.pformat(ast_node, indent="  ", show_offsets=True)
+        new_file = "ast_" + filename[:-3] + ".txt"
+        out_file = open(new_file, "w+")
+        out_file.write(pretty_string)
+        out_file.close()
+
+        
+        
